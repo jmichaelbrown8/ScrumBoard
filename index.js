10a6dd843a433b9b92794ca8b9f5e631439462ee
J. Michael Brown <jmichaelbrown8@gmail.com>
2016-01-12 22:23:33 -0800

    updated styles on pbi.html

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ var app = express();
 
 app.use('/api/v1', require('./api')(wagner));
 
+app.use(express.static('./', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
+
 app.listen(3000);
 console.log('Listening on port 3000!');
